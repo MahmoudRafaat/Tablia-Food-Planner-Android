a5f75d37757bbc73c48d0ae89dcc6027fce4af2c
Mahmoud Raafat <100778020+MahmoudRafaat@users.noreply.github.com>
2026-02-07 11:27:42 +0200

    added meals deatails presneter and ingredient adpater files

---
+++ app/src/main/java/com/example/tablia/presentation/meal_details/view/IngredientAdapter.java
@@ -0,0 +1,67 @@
+package com.example.tablia.presentation.meal_details.view;
+
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.tablia.R;
+
+import java.util.List;
+
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.ViewHolder> {
+
+    private List<Pair<String, String>> ingredients;
+
+    public IngredientAdapter(List<Pair<String, String>> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setList(List<Pair<String, String>> ingredients) {
+        this.ingredients = ingredients;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_ingredient, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Pair<String, String> ingredient = ingredients.get(position);
+        holder.tvName.setText(ingredient.first);
+        holder.tvMeasure.setText(ingredient.second);
+        
+        String imageUrl = "https://www.themealdb.com/images/ingredients/" + ingredient.first + ".png";
+        Glide.with(holder.itemView.getContext())
+                .load(imageUrl)
+                .placeholder(R.drawable.ic_launcher_foreground)
+                .into(holder.ivIngredient);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredients == null ? 0 : ingredients.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivIngredient;
+        TextView tvName, tvMeasure;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivIngredient = itemView.findViewById(R.id.iv_ingredient);
+            tvName = itemView.findViewById(R.id.tv_ingredient_name);
+            tvMeasure = itemView.findViewById(R.id.tv_ingredient_measure);
+        }
+    }
+}
